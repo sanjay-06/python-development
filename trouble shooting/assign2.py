a56da5ca105685a5d83268a12c0bfd293d93734a
Sanjay T <59244273+sanjay-06@users.noreply.github.com>
2020-06-29 19:29:12 +0530

    Troubleshooting and debugging using python

---
+++ trouble shooting/assign2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from multiprocessing import Pool
+import multiprocessing
+import subprocess
+import os
+
+src = "data/prod/"
+dest = "data/prod_backup/"
+
+if __name__ == "__main__":
+  pool = Pool(multiprocessing.cpu_count())
+  pool.apply(subprocess.call, args=(["rsync", "-arq", src, dest],))
---
+++ trouble shooting/thumbnailgenerator.py
@@ -0,0 +1,45 @@
+from concurrent import futures
+
+import argparse
+import logging
+import os
+import sys
+
+import PIL
+import PIL.Image
+
+from tqdm import tqdm
+
+def process_options():
+    kwargs ={
+    'format' : '[%(levelname)s] %(message)s'
+    }
+    parser = argparse.ArgumentParser(
+    description='Thmbnail generator',
+    fromfile_prefix_chars='@'
+    )
+    parser.add_argument('--debug',action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+
+def progress_bar(files):
+    return tqdm(files,desc='Processing',total=len(files),dynamic_ncols=True)
+
+def main():
+    process_options()
+
+    if not os.path.exists('thumbnails'):
+          os.mkdir('thumbnails')
+
+    executor = futuress.ProcessPoolExecutor()
+
+    for root, _, files in os.walk('images'):
+        for basename in progress_bar(files):
+            if not basename.endswith('.jpg'):
+                continue
+        executor.submit(process_file,root,basename)
+    print('waiting for all threads to finish.')
+    executor.shutdown()
+    return 0
+
+if __name__ == "__main__"
+   sys.exit(main())
